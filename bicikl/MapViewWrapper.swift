a8c64d2bcac5c68b0736b8cad3767b8d7e8449ac
Heda Nikolić <heda@Hedas-MacBook-Pro.local>
2025-09-03 13:18:14 +0200

    Update of route paths

--- bicikl/MapViewWrapper.swift
+++ bicikl/MapViewWrapper.swift
@@ -50,30 +50,30 @@ struct MapViewWrapper: UIViewRepresentable {
         }
 
         
-        let routes: [(start: CLLocationCoordinate2D, end: CLLocationCoordinate2D)] = [
-            (start: CLLocationCoordinate2D(latitude: 45.56119, longitude: 18.68025), end: CLLocationCoordinate2D(latitude: 45.55241, longitude: 18.73033)), // Kapucinska - Zeleno polje
-            (start: CLLocationCoordinate2D(latitude: 45.55819, longitude: 18.68273), end: CLLocationCoordinate2D(latitude: 45.55446, longitude: 18.72192)), // Trznica - Trg bana J. J.
-            (start: CLLocationCoordinate2D(latitude: 45.56696, longitude: 18.66481), end: CLLocationCoordinate2D(latitude: 45.55830, longitude: 18.70706)), // Promenada(desna obala)
-            (start: CLLocationCoordinate2D(latitude: 45.56764, longitude: 18.66958), end: CLLocationCoordinate2D(latitude: 45.56335, longitude: 18.71748)), // Promenada(lijeva obala)
-            (start: CLLocationCoordinate2D(latitude: 45.54529, longitude: 18.69267), end: CLLocationCoordinate2D(latitude: 45.56134, longitude: 18.70059)), // Gradski vrt - Tudmanov most
-            (start: CLLocationCoordinate2D(latitude: 45.56273, longitude: 18.70186), end: CLLocationCoordinate2D(latitude: 45.59401, longitude: 18.74005)), // Tudmanov most - Bilje
-            (start: CLLocationCoordinate2D(latitude: 45.56034, longitude: 18.68564), end: CLLocationCoordinate2D(latitude: 45.55722, longitude: 18.69680)), // Stepinceva - Trpimirova
-            (start: CLLocationCoordinate2D(latitude: 45.54519, longitude: 18.68119), end: CLLocationCoordinate2D(latitude: 45.54677, longitude: 18.72901)), // Gacka - Divaltova
-            (start: CLLocationCoordinate2D(latitude: 45.55885, longitude: 18.66218), end: CLLocationCoordinate2D(latitude: 45.55727, longitude: 18.68585)), // Gundulićeva
-            (start: CLLocationCoordinate2D(latitude: 45.54202, longitude: 18.70901), end: CLLocationCoordinate2D(latitude: 45.55868, longitude: 18.70540)), // Svačićeva
-            (start: CLLocationCoordinate2D(latitude: 45.54241, longitude: 18.70895), end: CLLocationCoordinate2D(latitude: 45.54349, longitude: 18.71831)), // Opatijska
-            (start: CLLocationCoordinate2D(latitude: 45.54389, longitude: 18.69671), end: CLLocationCoordinate2D(latitude: 45.54286, longitude: 18.70154)), // Ul. Woodrowa Wilsona - Kutinska
-            (start: CLLocationCoordinate2D(latitude: 45.55422, longitude: 18.67573), end: CLLocationCoordinate2D(latitude: 45.54631, longitude: 18.67865)), // Vinkovačka
-            (start: CLLocationCoordinate2D(latitude: 45.54157, longitude: 18.64762), end: CLLocationCoordinate2D(latitude: 45.55112, longitude: 18.63393)), // Borova - Portanova
-            (start: CLLocationCoordinate2D(latitude: 45.55612, longitude: 18.63499), end: CLLocationCoordinate2D(latitude: 45.55729, longitude: 18.64051)), // Svilajska (uz Portanovu)
-            (start: CLLocationCoordinate2D(latitude: 45.55622, longitude: 18.67854), end: CLLocationCoordinate2D(latitude: 45.55823, longitude: 18.67843)), // Prolaz Ante Slaviceka
-            (start: CLLocationCoordinate2D(latitude: 45.54949, longitude: 18.69810), end: CLLocationCoordinate2D(latitude: 45.55214, longitude: 18.70159)) // Setaliste Joze Petrovica (Sjenjak)
+        let routes: [(start: CLLocationCoordinate2D, via: CLLocationCoordinate2D?, end: CLLocationCoordinate2D)] = [
+            (start: CLLocationCoordinate2D(latitude: 45.56119, longitude: 18.68025), via: nil, end: CLLocationCoordinate2D(latitude: 45.55241, longitude: 18.73033)), // Kapucinska - Zeleno polje
+            (start: CLLocationCoordinate2D(latitude: 45.55819, longitude: 18.68273), via: nil, end: CLLocationCoordinate2D(latitude: 45.55446, longitude: 18.72192)), // Trznica - Trg bana J. J.
+            (start: CLLocationCoordinate2D(latitude: 45.56696, longitude: 18.66481), via: nil, end: CLLocationCoordinate2D(latitude: 45.55830, longitude: 18.70706)), // Promenada(desna obala)
+            (start: CLLocationCoordinate2D(latitude: 45.56764, longitude: 18.66958), via: nil, end: CLLocationCoordinate2D(latitude: 45.56335, longitude: 18.71748)), // Promenada(lijeva obala)
+            (start: CLLocationCoordinate2D(latitude: 45.54529, longitude: 18.69267), via: nil, end: CLLocationCoordinate2D(latitude: 45.56134, longitude: 18.70059)), // Gradski vrt - Tudmanov most
+            (start: CLLocationCoordinate2D(latitude: 45.56273, longitude: 18.70186), via: nil, end: CLLocationCoordinate2D(latitude: 45.59401, longitude: 18.74005)), // Tudmanov most - Bilje
+            (start: CLLocationCoordinate2D(latitude: 45.56034, longitude: 18.68564), via: nil, end: CLLocationCoordinate2D(latitude: 45.55722, longitude: 18.69680)), // Stepinceva - Trpimirova
+            (start: CLLocationCoordinate2D(latitude: 45.54519, longitude: 18.68119), via: nil, end: CLLocationCoordinate2D(latitude: 45.54677, longitude: 18.72901)), // Gacka - Divaltova
+            (start: CLLocationCoordinate2D(latitude: 45.55885, longitude: 18.66218), via: nil, end: CLLocationCoordinate2D(latitude: 45.55727, longitude: 18.68585)), // Gundulićeva
+            (start: CLLocationCoordinate2D(latitude: 45.54202, longitude: 18.70901), via: nil, end: CLLocationCoordinate2D(latitude: 45.55868, longitude: 18.70540)), // Svačićeva
+            (start: CLLocationCoordinate2D(latitude: 45.54241, longitude: 18.70895), via: nil, end: CLLocationCoordinate2D(latitude: 45.54349, longitude: 18.71831)), // Opatijska
+            (start: CLLocationCoordinate2D(latitude: 45.54389, longitude: 18.69671), via: nil, end: CLLocationCoordinate2D(latitude: 45.54286, longitude: 18.70154)), // Ul. Woodrowa Wilsona - Kutinska
+            (start: CLLocationCoordinate2D(latitude: 45.55422, longitude: 18.67573), via: nil, end: CLLocationCoordinate2D(latitude: 45.54631, longitude: 18.67865)), // Vinkovačka
+            (start: CLLocationCoordinate2D(latitude: 45.54157, longitude: 18.64762), via: nil, end: CLLocationCoordinate2D(latitude: 45.55112, longitude: 18.63393)), // Borova - Portanova
+            (start: CLLocationCoordinate2D(latitude: 45.55603, longitude: 18.63502), via: CLLocationCoordinate2D(latitude: 45.55579, longitude: 18.64023), end: CLLocationCoordinate2D(latitude: 45.55729, longitude: 18.64051)), // Svilajska (uz Portanovu)
+            (start: CLLocationCoordinate2D(latitude: 45.55622, longitude: 18.67854), via: nil, end: CLLocationCoordinate2D(latitude: 45.55823, longitude: 18.67843)), // Prolaz Ante Slaviceka
+            (start: CLLocationCoordinate2D(latitude: 45.54949, longitude: 18.69810), via: nil, end: CLLocationCoordinate2D(latitude: 45.55214, longitude: 18.70159)) // Setaliste Joze Petrovica (Sjenjak)
         ]
         
         
         for (index, route) in routes.enumerated() {
             let routeName = routeData.routes[index].name
-            addRoute(to: mapView, from: route.start, to: route.end, withName: routeName)
+            addRoute(to: mapView, from: route.start, via: route.via, to: route.end, withName: routeName)
         }
 
         let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handleTapGesture(_:)))
@@ -86,31 +86,77 @@ struct MapViewWrapper: UIViewRepresentable {
         // Update the map view if needed
     }
 
-    func addRoute(to mapView: MKMapView, from sourceCoordinate: CLLocationCoordinate2D, to destinationCoordinate: CLLocationCoordinate2D, withName name: String) {
+    func addRoute(
+        to mapView: MKMapView,
+        from sourceCoordinate: CLLocationCoordinate2D,
+        via viaCoordinate: CLLocationCoordinate2D?,
+        to destinationCoordinate: CLLocationCoordinate2D,
+        withName name: String
+    ) {
         let sourcePlacemark = MKPlacemark(coordinate: sourceCoordinate)
         let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
-        
-        let request = MKDirections.Request()
-        request.source = MKMapItem(placemark: sourcePlacemark)
-        request.destination = MKMapItem(placemark: destinationPlacemark)
-        request.transportType = .walking
-        
-        let directions = MKDirections(request: request)
-        directions.calculate { response, error in
-            if let error = error {
-                print("Error calculating directions: \(error.localizedDescription)")
-                return
+        let sourceItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationItem = MKMapItem(placemark: destinationPlacemark)
+
+        func request(_ a: MKMapItem, _ b: MKMapItem) -> MKDirections.Request {
+            let r = MKDirections.Request()
+            r.source = a
+            r.destination = b
+            // Cycling isn't a dedicated type in MapKit; .walking usually snaps better to bike paths than .automobile.
+            r.transportType = .walking
+            r.requestsAlternateRoutes = false
+            return r
+        }
+
+        // If no via: do a single A→B request
+        guard let viaCoordinate = viaCoordinate else {
+            let directions = MKDirections(request: request(sourceItem, destinationItem))
+            directions.calculate { response, error in
+                if let error = error {
+                    print("Error calculating directions (A→B): \(error.localizedDescription)")
+                    return
+                }
+                guard let route = response?.routes.first else {
+                    print("No routes found (A→B)")
+                    return
+                }
+                let polyline = route.polyline
+                polyline.title = name
+                mapView.addOverlay(polyline)
             }
-            guard let route = response?.routes.first else {
-                print("No routes found")
-                return
+            return
+        }
+
+        // With via: do two requests A→via and via→B
+        let viaItem = MKMapItem(placemark: MKPlacemark(coordinate: viaCoordinate))
+
+        let d1 = MKDirections(request: request(sourceItem, viaItem))
+        d1.calculate { res1, err1 in
+            if let err1 = err1 {
+                print("Error calculating directions (A→via): \(err1.localizedDescription)")
+            } else if let r1 = res1?.routes.first {
+                let pl1 = r1.polyline
+                pl1.title = name
+                mapView.addOverlay(pl1)
+            } else {
+                print("No routes found (A→via)")
             }
-            print("Route calculated: \(route)")
-            let polyline = route.polyline
-            polyline.title = name
-            mapView.addOverlay(polyline)
+
+            let d2 = MKDirections(request: request(viaItem, destinationItem))
+            d2.calculate { res2, err2 in
+                if let err2 = err2 {
+                    print("Error calculating directions (via→B): \(err2.localizedDescription)")
+                } else if let r2 = res2?.routes.first {
+                    let pl2 = r2.polyline
+                    pl2.title = name
+                    mapView.addOverlay(pl2)
+                } else {
+                    print("No routes found (via→B)")
+                }
+            }
         }
     }
+
 }
 // Setting the tap gesture for routes
 extension MapViewWrapper.Coordinator {
--- bicikl/RouteData.swift
+++ bicikl/RouteData.swift
@@ -16,22 +16,22 @@ struct Route: Identifiable {
 
 class RouteData: ObservableObject {
     @Published var routes: [Route] = [
-        Route(name: "Zeleno polje – Lučki prilaz", description: "4096 m Zeleno polje – M. Gupca – Trg B. J. Jelačića – Park K. K. Kosače – C. Hadrijana – Europska Avenija – Kapucinska – Lučki prilaz"),
-        Route(name: "Donji grad - Centar", description: "3250 m M. Gupca – Cvjetkova – Vukovarska – Gajev Trg – Radićeva"),
-        Route(name: "Promenada (desna obala)", description: "4381 m C. Hadrijana – ispod Mosta Dr. F. Tuđmana – Promenada – Solarski trg – J.J.Strossmayerova (do A. Kanižlića) – (od Kolodvorske – do Š. Petefija 620 m)"),
-        Route(name: "Promenada (lijeva obala)", description: "2815 m Promenadom od ZOO Hotela do istočnog ulaza/izlaza iz Copacabane i od zapadnog ulaza/zlaza iz Copacabane do željezničkog mosta na Dravi uključujući i pješački most na Dravi"),
-        Route(name: "Gradski vrt - Tuđmanov most", description: "2000 m Ulicom kneza Trpimira od Gradskog vrta do mosta Dr. F. Tuđmana (cestovni)"),
-        Route(name: "Biljska cesta", description: "1170 m Od mosta Dr. F. Tuđmana (cestovni), uz Biljski cestu do benzinske postaje na Biljskoj cesti i dalje prema Bilju"),
-        Route(name: "Tvrđa – gornjogradska tržnica", description: "1509 m Križanje Trpimirove – C. Hadrijana – Vijenac I. Meštrovića – Srednjoškolsko igralište – Zvonimirova – A. Stepinca (sudski prolaz) spoj na Europsku aveniju"),
-        Route(name: "Bosutsko naselje – Jug II", description: "3970 m Od Miljacke ul. – Gacka – M. Divalta – Velebitska (Jug II)"),
-        Route(name: "Gundulićeva", description: "2040 m Gajev trg – Trg B. Trenka – I. Gundulića (od A. Kanižlića)"),
-        Route(name: "Svačićeva - Tenja", description: "4590 m Ul. K. P. Svačića (od ul. C. Hadrijana) – nadvožnjak Rosinjača –  J.R. Kira – do Tenje"),
-        Route(name: "Jug II(Opatijska)", description: "1302 m Opatijska ul. (od K. P. Svačića  do Srijemske, obostrano na dionici od Trpanjske do Srijemske)"),
-        Route(name: "Ul. Woodrowa Wilsona - Kutinska", description: "A route from Stepinceva to Trpimirova."),
+        Route(name: "Zeleno polje – Lučki prilaz", description: "4096 m Zeleno polje – Ulica Matije Gupca – Trg bana J. Jelačića – Ulica Cara Hadrijana – Europska Avenija – Kapucinska – Lučki prilaz"),
+        Route(name: "Donji grad - Centar", description: "3250 m Ulica Matije Gupca – Cvjetkova ulica – Vukovarska ulica – Gajev Trg – Ulica Stjepana Radića"),
+        Route(name: "Promenada (desna obala)", description: "4381 m Ulica Cara Hadrijana – ispod Mosta dr. Franje Tuđmana – Promenada – Solarski trg – Ulica J.J.Strossmayera (do A. Kanižlića) – (od Kolodvorske – do Š. Petefija 620 m)"),
+        Route(name: "Promenada (lijeva obala)", description: "2815 m Promenadom od ZOO Hotela do istočnog ulaza/izlaza Copacabane i od zapadnog ulaza/izlaza Copacabane do željezničkog mosta na Dravi uključujući i pješački most na Dravi"),
+        Route(name: "Gradski vrt - Tuđmanov most", description: "2000 m Ulicom kneza Trpimira od Gradskog vrta do Mosta dr. Franje Tuđmana"),
+        Route(name: "Biljska cesta", description: "1170 m Od mosta dr. Franje Tuđmana, uz Biljsku cestu do benzinske postaje na Biljskoj cesti i dalje prema Bilju"),
+        Route(name: "Tvrđa – gornjogradska tržnica", description: "1509 m Srednjoškolsko igralište – Ulica kralja Zvonimira – Ulica Alojzija Stepinca (sudski prolaz), spoj na Europsku aveniju"),
+        Route(name: "Bosutsko naselje – Jug II", description: "3970 m Gacka (od Miljacke ulice) – Ulica Maritna Divalta – Velebitska ulica (Jug II)"),
+        Route(name: "Gundulićeva", description: "2040 m Gajev trg – Trg baruna Trenka – Ulica Ivana Gundulića (od A. Kanižlića)"),
+        Route(name: "Svačićeva - Tenja", description: "4590 m Ulica kralja P. Svačića (od ul. C. Hadrijana) – nadvožnjak Rosinjača – Tenjska ulica"),
+        Route(name: "Jug II (Opatijska)", description: "1302 m Opatijska ul. (od Ulice kralja P. Svačića do Srijemske, obostrano na dionici od Trpanjske do Srijemske)"),
+        Route(name: "Uske njive", description: " 500 m Delnička ulica - Novogradiška ulica - Lipička ulica do Kutinske ulice"),
         Route(name: "Vinkovačka", description: "1440 m Od kružnog toka Đakovština do Dunavske ul. obostrano, od Dunavske ul. sa zapadne strane do kružnog toka Drinska-Bosutska (Mačkamama)"),
-        Route(name: "Borova - Portanova", description: "A route from Stepinceva to Trpimirova."),
-        Route(name: "Svilajska (uz Portanovu)", description: "A route from Stepinceva to Trpimirova."),
-        Route(name: "Prolaz Ante Slavičeka", description: " 230 m Od ul. I. Gundulića do ul. Hrvatske Republike"),
-        Route(name: "Šetalište Joze Petroviča (Sjenjak)", description: "A route from Stepinceva to Trpimirova.")
+        Route(name: "Borova - Portanova", description: "1700 m Uz Gospodarsku zonu od Borove ulice do Portanove"),
+        Route(name: "Portanova", description: " 550 m Od Svilajske ulice uz Portanovu"),
+        Route(name: "Prolaz Ante Slavičeka", description: " 230 m Od Ulice Ivana Gundulića do Ulice Hrvatske Republike"),
+        Route(name: "Šetalište Joze Petroviča (Sjenjak)", description: " 650 m Sjenjak (od Robne kuće) do Ulice Ivana Zajca i spoj prema biciklističkoj stazi na Ulici Martina Divalta")
     ]
 }
